4020ad18b92d867aff4126ae9487232d2b28dc7b
Jessamy Ventura <jessamy_ven@outlook.com>
2025-01-16 17:48:01 -0600

    mi proyecto

---
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+class ButtonsScreen
+ extends StatelessWidget {
+  const ButtonsScreen
+  ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons Screen'),
+      ),
+      body: _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: (){
+          context.pop();
+        },
+        
+        ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+       final colors= Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20) ,
+        child: Wrap(
+          spacing: 10,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: const Text('Elevated')),
+            const ElevatedButton(onPressed: null, child:  Text('Elevated Disabled')),
+      
+            ElevatedButton.icon(
+            onPressed: (){}, icon: const Icon( Icons.access_alarm_rounded),
+             label: Text('Elevated Icon'),
+            ),
+
+             FilledButton(onPressed: (){}, child: const Text('Filled')),
+             FilledButton.icon (onPressed: (){}, 
+             icon: const Icon(Icons.accessibility_outlined),
+             label: const Text('Filled Icon'),
+
+             ),
+
+             OutlinedButton(onPressed: (){}, child: const Text('Qutline')),
+             OutlinedButton.icon (
+              onPressed: (){}, 
+              label: const Text('Outline Icon'),
+              icon: Icon(Icons.terminal)),
+               TextButton(onPressed: (){}, 
+               child: const Text('Deporte')),
+               TextButton.icon (onPressed: (){},
+                icon:const Icon(Icons.account_box_outlined),
+                label: const Text('Datos'),),
+                
+                const CustomButton(),
+
+                IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
+                IconButton(onPressed: (){},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(colors.primary),
+                  iconColor: const MaterialStatePropertyAll(Colors.white),
+
+                ),
+                 icon: const Icon(Icons.app_registration_rounded)),
+                
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+
+      child: Material(
+        color: Colors.purpleAccent,
+        child: InkWell(
+         
+          onTap: (){},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('hola mundo',style: TextStyle(color:Colors.white),)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/infinite_scroll/infinite_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScreen extends StatelessWidget {
+
+
+  static const name= 'infinite_screen';
+
+
+  const InfiniteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
